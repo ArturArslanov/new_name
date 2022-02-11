d91ab6d2ae27f9ad12d809d9d4bf4a5d51032ca5
Artur <aat281205@gmail.com>
2022-02-11 19:18:00 +0300

    Initial commit

---
+++ web6.py-master/funcs.py
@@ -0,0 +1,19 @@
+import requests as req
+
+
+def find_spn(address):
+    quer = "https://search-maps.yandex.ru/v1/"
+    api_key = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+    params = {
+        "apikey": api_key,
+        "text": address,
+        "lang": "ru_RU",
+        "results": '1',
+    }
+    response = req.get(quer, params=params)
+    if not response:
+        raise AttributeError(response.content)
+    res = response.json()
+    coords = res['features'][0]['properties']['boundedBy']
+    spn = max(abs(coords[1][0] - coords[0][0]), abs(coords[1][1] - coords[0][1]))
+    return spn
